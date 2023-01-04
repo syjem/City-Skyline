b81e489a6337e8ddac9f5215cad219874d24cfb2
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-01-04 13:50:59 +0800

    bb2a

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const button = document.querySelector('#myButton');
 const sky = document.querySelector('#sky');
 const bb1 = document.querySelector('.bb1-window');
+const bb1d = document.querySelector('.bb1d');
+const bb2a = document.querySelector('.bb2a');
 let colors = false;
 
 
@@ -13,10 +15,14 @@ button.addEventListener('click', () => {
     if (colors) {
         sky.style.background = 'var(--sky-color)';
         bb1.style.background = 'linear-gradient(var(--building-color1), var(--window-color1))';
+        bb1d.style.background = 'linear-gradient(var(--building-color1) 50%, var(--window-color1))';
+        bb2a.style.borderBottom = '5vh solid var(--building-color2)';
     }
     else {
         sky.style.background = 'var(--sky-color-dark)';
         bb1.style.background = 'linear-gradient(#000, #777)';
+        bb1d.style.background = 'linear-gradient(#000 50%, #777)';
+        bb2a.style.borderBottom = '5vh solid #000';
     }
     colors = !colors;
     
