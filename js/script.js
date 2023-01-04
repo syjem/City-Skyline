61e239ab7d3ef9dea7788a20583c311a12941bd8
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-01-04 14:21:51 +0800

    deleted

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
 
 const button = document.querySelector('#myButton');
 const sky = document.querySelector('#sky');
-const bb1 = document.querySelector('.bb1-window');
-const bb1d = document.querySelector('.bb1d');
-const bb2a = document.querySelector('.bb2a');
-const bb2b = document.querySelector('.bb2b ');
-const bb3 = document.querySelector('.bb3');
-const bb4a = document.querySelector('.bb4a');
-const bb4b = document.querySelector('.bb4b');
-const bb4c = document.querySelector('.bb4c');
-const bb4Window = document.querySelector('.bb4-window');
 let colors = false;
 
 
