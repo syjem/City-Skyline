fca610b7312b356f38786eca9fa25a9789beab06
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-01-04 14:21:28 +0800

    completed

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ const bb2a = document.querySelector('.bb2a');
 const bb2b = document.querySelector('.bb2b ');
 const bb3 = document.querySelector('.bb3');
 const bb4a = document.querySelector('.bb4a');
+const bb4b = document.querySelector('.bb4b');
+const bb4c = document.querySelector('.bb4c');
+const bb4Window = document.querySelector('.bb4-window');
 let colors = false;
 
 
@@ -17,21 +20,9 @@ button.addEventListener('click', () => {
 
     if (colors) {
         sky.style.background = 'var(--sky-color)';
-        bb1.style.background = 'linear-gradient(var(--building-color1), var(--window-color1))';
-        bb1d.style.background = 'linear-gradient(var(--building-color1) 50%, var(--window-color1))';
-        bb2a.style.borderBottom = '5vh solid var(--building-color2)';
-        bb2b.style.background = 'repeating-linear-gradient(var(--building-color2),var(--building-color2) 6%,var(--window-color2) 6%,var(--window-color2) 9%)';
-        bb3.style.background = 'repeating-linear-gradient(90deg,var(--building-color3),var(--building-color3),var(--window-color3) 15%';
-        bb4a.style.backgroundColor = 'var(--building-color4)';
     }
     else {
         sky.style.background = 'var(--sky-color-dark)';
-        bb1.style.background = 'linear-gradient(#000, #777)';
-        bb1d.style.background = 'linear-gradient(#000 50%, #777)';
-        bb2a.style.borderBottom = '5vh solid #000';
-        bb2b.style.background = 'repeating-linear-gradient(#000, #000 6%, #777 6%, #777 9%)';
-        bb3.style.background = 'repeating-linear-gradient(90deg, #000, #000, #777 15%';
-        bb4a.style.backgroundColor = '#000';
     }
     colors = !colors;
     
